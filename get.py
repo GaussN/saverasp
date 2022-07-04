160156c4d6bc4d6e900a77a86a1c4aa41b14ee95
GaussN <barbaradob17@gmail.com>
2022-07-04 16:03:30 +0300

    create rep

---
+++ get.py
@@ -0,0 +1,17 @@
+#function to get couple schedule
+
+from requests import get  
+from bs4 import BeautifulSoup as parser
+
+mgke = 'http://mgke.minsk.edu.by/ru/main.aspx?guid=3831'
+
+def getSchedule():
+    try:
+        response = get(mgke)
+        soup = parser(response.text, 'html')
+        return soup.table
+    except Exception as e:
+        print('exception: ', e)
+
+if __name__ == '__main__': 
+    print(getSchedule())
---
+++ parser.py
@@ -0,0 +1,9 @@
+#
+
+def findFunking44GRoup():
+    rasp = "{\n"
+    
+    return rasp + "}"
+
+if __name__ == '__main__':
+    print(findFunking44GRoup())
