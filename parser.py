6073fe7584004c2a52ebee365f28a67caff35e91
GaussN <barbaradob17@gmail.com>
2022-07-05 22:04:50 +0300

    sex with .gitignore

--- parser.py
+++ parser.py
@@ -3,18 +3,26 @@
 import re
 from get import getSchedule
 
-def findFucking44Group():
-    trs = getSchedule().findAll('tr')
+GROUP = '46'
+
+def findFuckingGroup():
+    table_rows = getSchedule().findAll('tr')
     i = 0
-    while i < len(trs):
-        date = re.search(r"\d{1,2}\.\d{1,2}\.\d{2,4}", trs[i].prettify())
+    while i < len(table_rows):
+        date = re.search(r"\d{1,2}\.\d{1,2}\.\d{2,4}", table_rows[i].prettify())
         if date is not None:
             #после строки с датой идёт строка с номерами групп
-            i += 1 #указывает на строку с номерами групп
-            groups = re.findall(r"<strong>(\d+)</strong>", trs[i].prettify());
-
-            print(groups)
-            pass
+            #i указывает на строку с номерами групп
+            i += 1 
+            groups = re.findall(r"<strong>\s*(\d+)\s*</strong>", table_rows[i].prettify());
+            if GROUP in groups:
+                column = groups.index(GROUP)
+                #после строки с номерами групп идес строка с бесполезным шлаком
+                i += 2
+                while (re.search(r'\d{1,2}\.\d{1,2}\.\d{2,4}', table_rows[i].prettify()) is not None) and (i < len(table_rows)):
+                    print('para\n', table_rows[i].findAll('td')[1].prettify())
+                    i += 1
+                break
         i += 1
     
 def createJSON():
@@ -24,8 +32,6 @@ def createJSON():
 
 
 if __name__ == '__main__':
-    #print(getSchedule().findAll('tr')[3])
-    #print(findFucking44Group())
-    findFucking44Group()
+    findFuckingGroup()
 
     pass
