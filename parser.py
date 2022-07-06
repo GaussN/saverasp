7e8e6df8f2283a9e521616c4292f11ddd8215aa2
GaussN <barbaradob17@gmail.com>
2022-07-06 08:35:22 +0300

    added creating a dictionary with pairs for the selected group

--- parser.py
+++ parser.py
@@ -6,9 +6,11 @@ from get import getSchedule
 GROUP = '46'
 
 def findFuckingGroup():
+    result = {}
     table_rows = getSchedule().findAll('tr')
     i = 0
     while i < len(table_rows):
+        result = {}
         date = re.search(r"\d{1,2}\.\d{1,2}\.\d{2,4}", table_rows[i].prettify())
         if date is not None:
             #после строки с датой идёт строка с номерами групп
@@ -19,18 +21,21 @@ def findFuckingGroup():
                 column = groups.index(GROUP)
                 #после строки с номерами групп идес строка с бесполезным шлаком
                 i += 2
-                while (re.search(r'\d{1,2}\.\d{1,2}\.\d{2,4}', table_rows[i].prettify()) is not None) and (i < len(table_rows)):
-                    print('para\n', table_rows[i].findAll('td')[1].prettify())
+                while (re.search(r'\d{1,2}\.\d{1,2}\.\d{2,4}', table_rows[i].prettify()) is None) and (i < len(table_rows)):
+                    table_data = table_rows[i].findAll('td')
+                    nomera_par = table_data[0::3]
+                    pary = table_data[2::3]
+                    nomera_kab = table_data[1::3]
+                    #result: словарь; ключ - номер пары, значение - словарь(ключ - пара с преподом, значение - номер кабинета)
+                    result[str(nomera_par[0])] =  { pary[0] : nomera_kab[0] }
+
+                    print({ nomera_par[0] : { pary[0] : nomera_kab[0] } })
+
                     i += 1
                 break
         i += 1
+    print("result:\n\n", result)
     
-def createJSON():
-    rasp = "{\n"
-    
-    return rasp + "}"
-
-
 if __name__ == '__main__':
     findFuckingGroup()
 
