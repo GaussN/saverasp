0be555d5cc3dd1dd752dca76e3b7385d784c1c2d
GaussN <barbaradob17@gmail.com>
2022-07-10 20:16:10 +0300

    the main loop is running on another thread

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import json
-from time import sleep
-from datetime import datetime, date
 import sys
-from pkg_resources import parse_requirements
-from get import getSchedule
-from parser import findGroupSchedule
+import json
 import threading
+from time import sleep
+from get import getSchedule
+from datetime import datetime, date
+from pkg_resources import parse_requirements
+from parser import findGroupSchedule
 
 # SUCCESS_CHECK_UPDATE_TIME = 43200   # время сна после удачного обновления расписания
 SUCCESS_CHECK_UPDATE_TIME = 60 * 5  # время сна после удачного обновления расписания
 FAILURE_CHECK_UPDATE_TIME = 1800  # время сна после неудачного обновления расписания
-FIND_GROUP = 46
+FIND_GROUP = 44
 
 def main():  
     schedule_old = ''
@@ -29,13 +29,16 @@ def main():
                 print(now, 'new schedule')
 
                 schedule_json = json.dumps(schedule_dict, ensure_ascii=False)
+                delay_time = SUCCESS_CHECK_UPDATE_TIME
 
                 # запись в файл
                 with open(f'output/{date.today()}.json', 'w+', encoding='utf-8') as file:
                     file.write(schedule_json)
                     print(now, 'schedule successfully written to file')
+            else:
+                print('no schedule found for the selected group')
+                delay_time = FAILURE_CHECK_UPDATE_TIME
 
-            delay_time = SUCCESS_CHECK_UPDATE_TIME
 
         else:
             print(now, 'nothing new')
@@ -52,5 +55,22 @@ if __name__ == '__main__':
 
     while True:
         cmd = input()
-        if cmd in ['e', 'exit', 'quit', 'stop']:
+        if cmd.strip() in ['e', 'exit']:
             sys.exit(0)
+        elif cmd.split(' ')[0] == 'set':
+            argv = cmd.split(' ')
+            try:
+                #sunccess update time
+                if argv[1] == 'SUT':
+                    SUCCESS_CHECK_UPDATE_TIME = int(argv[2])
+                #failure update time
+                elif argv[1] == 'FUT':
+                    FAILURE_CHECK_UPDATE_TIME = int(argv[2])
+            except Exception as ex:
+                print(ex)
+        elif cmd == 'help':
+            print('''
+Type: "e" or "exit" to stop the process
+Type "help" to get help
+Type "set (SUT/FUT) VAL" to set value SUCCESS_CHECK_UPDATE_TIME/FAILURE_CHECK_UPDATE_TIME
+''')
